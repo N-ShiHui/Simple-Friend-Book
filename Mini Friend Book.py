c7714d64f0ef2ce8e7b5b90ca89bc2e07bedc74a
N-ShiHui <161719020+N-ShiHui@users.noreply.github.com>
2025-03-20 12:24:49 +0800

    Updated mini friend book

---
+++ Mini Friend Book.py
@@ -0,0 +1,77 @@
+namebook = {"name": [], "hobby": [], "age": []}
+
+while True:
+    print("What would you like to do?\n1. Add new user\n2. Display current users\n3. Remove user\n4. Search user info\n5. Exit")
+
+    choice = input("Select your option:")
+
+    if choice == '1':
+        # Capture new user information
+        name = input("What is your name?")
+        hobby = input("What is your favourite hobby?")
+        age = input("What is your age?")
+
+        # Append new user information to namebook
+        namebook["name"].append(name),
+        namebook["hobby"].append(hobby),
+        namebook["age"].append(age)
+
+        print("User updated successfully.")
+
+    elif choice == '2':
+        # Display current users:
+        if not namebook["name"]:
+            print("User not found in system.")
+        else:
+            print("Current users:")
+            for i, (name, hobby, age) in enumerate(zip(namebook["name"],
+                                                       namebook["hobby"],
+                                                       namebook["age"])):
+                print(f"{i+1}. {name}\n{'   '}Hobby: {hobby}\n{'   '}Age: {age}\n")
+
+    elif choice == '3':
+        if not namebook["name"]:
+            print("No user to remove.")
+        else:
+            print("Current users:")
+            for i, (name, hobby, age) in enumerate(zip(namebook["name"], 
+                                                       namebook["hobby"], 
+                                                       namebook["age"])):
+                   print(f"{i+1}. {name}, {hobby}, {age}")
+            try:
+                index_to_remove = int(input("Enter the index of the user:")) - 1
+                if 0 <= index_to_remove < len(namebook["name"]):
+                    del namebook["name"][index_to_remove]
+                    del namebook["hobby"][index_to_remove]
+                    del namebook["age"][index_to_remove]
+                    print("User removed successfully.")
+                else:
+                    print("Invalid index. Please enter a valid index.")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+                
+    elif choice == '4':
+        search_item = input("Enter user name: ").strip().lower()
+        found_users = []
+        for i in range(len(namebook["name"])):
+            if namebook["name"][i].lower() == search_item:
+                found_users.append({
+                    "Name": namebook["name"][i],
+                    "Hobby": namebook['hobby'][i],
+                    "Age": namebook['age'][i]
+                })
+        
+        if found_users:
+            print("Users found:".format(search_item))
+            for user in found_users:
+                print(f"Name: {user['Name']}, Hobby: {user['Hobby']}, Age: {user['Age']}")
+        else:
+            print("No such existing user.")
+
+    elif choice == '5':
+        # Exit the program
+        print("Exiting the system. Goodbye!")
+        break
+
+    else:
+        print("Invalid choice. Please try again.")
